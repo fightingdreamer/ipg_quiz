167f4e722e5b51710ce6e6f81a94a13ce4bdc514
[IgnetStudio] <ignet.studio@gmail.com>
2020-10-09 13:44:56 +0200

    [ADD] React App with TypeScript, Prettier config & node-sass

---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+// packages
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// components & js
+
+import App from './App';
+
+// code
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
