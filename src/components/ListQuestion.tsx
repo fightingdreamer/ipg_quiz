ff70bfa619f8f2152ad9ad92a97257928437f616
[IgnetStudio] <ignet.studio@gmail.com>
2020-10-14 00:18:15 +0200

    [ADD] ListQuestion: hook & conditions for result

--- src/components/ListQuestion.tsx
+++ src/components/ListQuestion.tsx
@@ -10,21 +10,32 @@ import { getQuestion } from '../utils/getQuestion';
 
 export const ListQuestion = () => {
   const [currentQuestion, updateQuestion] = useState(0);
+  const currentResult = useState(0);
   const objectQuestion = getQuestion[currentQuestion];
   const handleQuestion = () => {
     const nextQuestion = currentQuestion + 1;
-    updateQuestion(nextQuestion);
+    if (nextQuestion < getQuestion.length) {
+      updateQuestion(nextQuestion);
+    } else {
+      console.log('game over');
+    }
   };
 
   return (
     <>
-      <h2>{objectQuestion.definition}</h2>
-      <ul>
-        {objectQuestion.options.map((question) => (
-          <li key={question.id}>{question.answer}</li>
-        ))}
-      </ul>
-      <button onClick={handleQuestion}>Button placeholder</button>
+      {currentResult ? (
+        <h2>Result placeholder</h2>
+      ) : (
+        <div>
+          <h2>{objectQuestion.definition}</h2>
+          <ul>
+            {objectQuestion.options.map((question) => (
+              <li key={question.id}>{question.answer}</li>
+            ))}
+          </ul>
+          <button onClick={handleQuestion}>Button placeholder</button>
+        </div>
+      )}
     </>
   );
 };
