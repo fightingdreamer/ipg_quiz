30284c31504342adaa4ce33aa41669c50157cf69
[IgnetStudio] <ignet.studio@gmail.com>
2020-10-14 15:37:13 +0200

    [FIX] ListQuestion: hooks, answer selection & handler

--- src/components/ListQuestion.tsx
+++ src/components/ListQuestion.tsx
@@ -4,49 +4,57 @@ import React, { useState } from 'react';
 
 // utils
 
-import { getQuestion } from '../utils/getQuestion';
+import { OptionQuestion, getQuestion } from '../utils/getQuestion';
 
 // code
 
 export const ListQuestion = () => {
-  // questions, result & score initial states
-  const [currentQuestion, updateQuestion] = useState(0);
-  const [currentResult, updateResult] = useState(0);
-  const [currentScore, updateScore] = useState(0);
+  // hooks default
+  const [currentScore, setScore] = useState(0);
+  const [currentQuestion, setQuestion] = useState(0);
+  const [currentStatus, setStatus] = useState(false);
+  const [currentCheck, setCheck] = useState({} as OptionQuestion);
   const objectQuestion = getQuestion[currentQuestion];
 
-  // questions, result & score handler
-  const handleQuestion = (arg) => {
-    if (arg) {
-      updateScore(currentScore + 1);
+  // hooks update
+  const handleQuestion = (correctAnswer: boolean) => {
+    if (correctAnswer) {
+      setScore(currentScore + 1);
     }
 
     const nextQuestion = currentQuestion + 1;
     if (nextQuestion < getQuestion.length) {
-      updateQuestion(nextQuestion);
+      setQuestion(nextQuestion);
     } else {
-      updateResult(1);
-      console.log('game over');
+      setStatus(true);
     }
   };
 
   return (
     <>
-      {currentResult ? (
-        <h2>Game over, score: {currentScore}</h2>
+      {currentStatus ? (
+        <h2>
+          Game over, score: {currentScore} out of {getQuestion.length}
+        </h2>
       ) : (
         <div>
           <h2>
-            # {currentQuestion + 1} {objectQuestion.definition}
+            question # {currentQuestion + 1} {objectQuestion.definition}
           </h2>
           <ul>
             {objectQuestion.options.map((question) => (
               <li key={question.id}>
+                <input
+                  type="radio"
+                  value={'option-' + question.id}
+                  checked={question.id === currentCheck.id}
+                  onChange={() => setCheck(question)}
+                />
                 {' # id:'} {question.id} {question.answer}
               </li>
             ))}
           </ul>
-          <button onClick={handleQuestion}>Button placeholder</button>
+          <button onClick={() => handleQuestion(currentCheck.value)}>Button placeholder</button>
         </div>
       )}
     </>
