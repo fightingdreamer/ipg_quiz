480888fd8a53ea5c3909c3efca89851157201c05
[IgnetStudio] <ignet.studio@gmail.com>
2020-10-14 13:00:39 +0200

    [FIX] definition & answer values / [ADD] result & score hooks

--- src/components/ListQuestion.tsx
+++ src/components/ListQuestion.tsx
@@ -9,14 +9,23 @@ import { getQuestion } from '../utils/getQuestion';
 // code
 
 export const ListQuestion = () => {
+  // questions, result & score initial states
   const [currentQuestion, updateQuestion] = useState(0);
-  const currentResult = useState(0);
+  const [currentResult, updateResult] = useState(0);
+  const [currentScore, updateScore] = useState(0);
   const objectQuestion = getQuestion[currentQuestion];
-  const handleQuestion = () => {
+
+  // questions, result & score handler
+  const handleQuestion = (arg) => {
+    if (arg) {
+      updateScore(currentScore + 1);
+    }
+
     const nextQuestion = currentQuestion + 1;
     if (nextQuestion < getQuestion.length) {
       updateQuestion(nextQuestion);
     } else {
+      updateResult(1);
       console.log('game over');
     }
   };
@@ -24,13 +33,17 @@ export const ListQuestion = () => {
   return (
     <>
       {currentResult ? (
-        <h2>Result placeholder</h2>
+        <h2>Game over, score: {currentScore}</h2>
       ) : (
         <div>
-          <h2>{objectQuestion.definition}</h2>
+          <h2>
+            # {currentQuestion + 1} {objectQuestion.definition}
+          </h2>
           <ul>
             {objectQuestion.options.map((question) => (
-              <li key={question.id}>{question.answer}</li>
+              <li key={question.id}>
+                {' # id:'} {question.id} {question.answer}
+              </li>
             ))}
           </ul>
           <button onClick={handleQuestion}>Button placeholder</button>
--- src/utils/getQuestion.tsx
+++ src/utils/getQuestion.tsx
@@ -12,23 +12,23 @@ export interface ArrayQuestion {
 
 export const getQuestion: ArrayQuestion[] = [
   {
-    definition: '1st item question definition placeholder',
+    definition: 'definition placeholder',
     id: 'q1',
     options: [
-      { answer: '1st item answer placeholder 1', value: false, id: '1' },
-      { answer: '1st item answer placeholder 2', value: true, id: '2' },
-      { answer: '1st item answer placeholder 3', value: false, id: '3' },
-      { answer: '1st item answer placeholder 4', value: false, id: '4' },
+      { answer: 'answer placeholder', value: false, id: '1' },
+      { answer: 'answer placeholder', value: true, id: '2' },
+      { answer: 'answer placeholder', value: false, id: '3' },
+      { answer: 'answer placeholder', value: false, id: '4' },
     ],
   },
   {
-    definition: '2nd item question definition placeholder',
+    definition: 'definition placeholder',
     id: 'q2',
     options: [
-      { answer: '2nd item answer placeholder 1', value: false, id: '5' },
-      { answer: '2nd item answer placeholder 2', value: false, id: '6' },
-      { answer: '2nd item answer placeholder 3', value: false, id: '7' },
-      { answer: '2nd item answer placeholder 4', value: true, id: '8' },
+      { answer: 'answer placeholder', value: false, id: '5' },
+      { answer: 'answer placeholder', value: false, id: '6' },
+      { answer: 'answer placeholder', value: false, id: '7' },
+      { answer: 'answer placeholder', value: true, id: '8' },
     ],
   },
 ];
