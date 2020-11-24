edbba4136568a6de2de17e1e5f248abec71dc0cc
[IgnetStudio] <ignet.studio@gmail.com>
2020-11-24 02:01:41 +0100

    [FIX] elements alignment

--- src/pages/PageQuiz.tsx
+++ src/pages/PageQuiz.tsx
@@ -4,24 +4,17 @@ import React from 'react';
 
 // components
 
+import ThemeRadio from '../components/ThemeRadio';
 import { ListQuestion } from '../components/ListQuestion';
-
-// theme switcher
-
-import { ThemeSwitcher } from '../components/ThemeSwitcher';
-import { ThemeSwitcherProvider } from 'react-css-theme-switcher';
-const themes = {
-  Terminal: 'path/placeholder.css',
-};
 
 // code
 
 export function PageQuiz() {
   return (
     <>
-      <ThemeSwitcherProvider defaultTheme="Terminal" themeMap={themes}>
-        <ThemeSwitcher />
-      </ThemeSwitcherProvider>
+      <div className="quiz-theme">
+        <ThemeRadio />
+      </div>
       <div className="quiz-noise"></div>
       <div className="quiz-overlay"></div>
       <ListQuestion />
