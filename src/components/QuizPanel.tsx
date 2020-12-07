662d679373c137fa70ccfb2beee6a636dab9f218
[IgnetStudio] <ignet.studio@gmail.com>
2020-12-07 06:37:53 +0100

    [FIX] UI-Material display: top panel, inputs & icons

--- src/components/QuizPanel.tsx
+++ src/components/QuizPanel.tsx
@@ -8,6 +8,7 @@ import TimerTwoToneIcon from '@material-ui/icons/TimerTwoTone';
 import ArrowRightTwoToneIcon from '@material-ui/icons/ArrowRightTwoTone';
 import HelpTwoToneIcon from '@material-ui/icons/HelpTwoTone';
 import AccountCircleTwoToneIcon from '@material-ui/icons/AccountCircleTwoTone';
+import FullscreenTwoToneIcon from '@material-ui/icons/FullscreenTwoTone';
 
 // code
 
@@ -23,7 +24,8 @@ export const QuizPanel = ({
   return (
     <div className="quiz-panel">
       <div className="quiz-user">
-        <AccountCircleTwoToneIcon fontSize="default" className="quiz-user" />
+        <AccountCircleTwoToneIcon fontSize="default" className="quiz-avatar" />
+        <span className="quiz-player">placeholder</span>
       </div>
       <div className="quiz-counter">
         <span>{setCurrentTime}</span>
@@ -31,6 +33,7 @@ export const QuizPanel = ({
         <TimerTwoToneIcon fontSize="default" className="quiz-timer" />
       </div>
       <div className="quiz-status">
+        <FullscreenTwoToneIcon fontSize="default" className="quiz-screen" />
         <HelpTwoToneIcon fontSize="default" className="quiz-help" />
         <span>{currentQuestion + 1}</span>
         <ArrowRightTwoToneIcon fontSize="default" className="quiz-arrow" />
